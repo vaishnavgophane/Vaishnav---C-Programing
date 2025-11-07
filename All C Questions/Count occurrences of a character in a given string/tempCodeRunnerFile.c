42a084c4e3ee4cb07a8dadaec120f3d501822891
vaishnavgophane <mr.vaishnavgophane@gmail.com>
2025-11-07 19:45:09 +0530

    Practicing C Programming language

---
+++ All C Questions/Count occurrences of a character in a given string/tempCodeRunnerFile.c
@@ -0,0 +1,12 @@
+scanf("%c",&s);
+
+    
+    // for(int i=0;c[i]!='\0';i++)
+    // {
+    //     if(s == c[i])
+    //     {
+    //         cnt++;
+    //     }
+    // }
+    // printf("Character %c Occured %d times.",s,cnt);
+    // return 0;
